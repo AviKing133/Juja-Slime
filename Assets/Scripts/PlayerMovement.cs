6aba2f1b693cd22b3f1669adc2dae8aaea0a5027
Victor <carusvictor66@gmail.com>
2026-01-13 11:36:06 +0100

    Primer Commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using System.Net.Sockets;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void MovePlayer(Vector3 direction, float speed)
+    {
+        
+    }
+}
